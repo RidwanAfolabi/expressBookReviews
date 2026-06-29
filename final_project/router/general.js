4af9c14543ae3352a580692ebae435d6a01c3b6b
Mr. RAM <69659931+RidwanAfolabi@users.noreply.github.com>
2026-06-29 16:54:31 +0800

    Update other get methods in general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,6 +7,14 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
+  users = {};
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+  };
+
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
@@ -29,20 +37,64 @@ public_users.get('/isbn/:isbn',function (req, res) {
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   const author = req.params.author;
-  //return res.send(JSON.stringify(books.author[author], null, 2));
-  //return res.status(300).json({message: "Yet to be implemented"});
+
+  // Hint 1: Obtain all the keys for the 'books' object
+  const keys = Object.keys(books);
+  
+  // array to store books that match the author
+  let booksByAuthor = [];
+
+  // Hint 2: Iterate through the keys and check if the author matches
+  for (let i = 0; i < keys.length; i++) {
+    let currentBook = books[keys[i]];
+    
+    // If the author matches, add the book to results array
+    if (currentBook.author === author) {
+      booksByAuthor.push(currentBook);
+    }
+  }
+
+  // Check if we found any books and return the appropriate response
+  if (booksByAuthor.length > 0) {
+    return res.status(200).send(JSON.stringify(booksByAuthor, null, 4));
+  } else {
+    return res.status(404).json({ message: "No books found by this author" });
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+
+  const keys = Object.keys(books);
+
+  // array to store books that match the author
+  let booksByTitle = [];
+
+  for (let i = 0; i < keys.length; i++) {
+    let currentBook = books[keys[i]];
+    
+    // If the author matches, add the book to results array
+    if (currentBook.title === title) {
+      booksByTitle.push(currentBook);
+    }
+  }
+
+  // Check if we found any books and return the appropriate response
+  if (booksByTitle.length > 0) {
+    return res.status(200).send(JSON.stringify(booksByTitle, null, 4));
+  } else {
+    return res.status(404).json({ message: "No books found by this title" });
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  return res.send(JSON.stringify(books[isbn].reviews, null, 2));
 });
 
 module.exports.general = public_users;
